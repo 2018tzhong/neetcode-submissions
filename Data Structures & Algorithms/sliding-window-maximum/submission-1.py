a1a6125eb30f60eab1493e7a9832f48f8101bfc1
Tim Zhong <73036507+2018tzhong@users.noreply.github.com>
2026-04-29 13:22:18 -0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,16 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        sl = SortedList()
+        results = []
+        l = 0
+        
+        for r, num in enumerate(nums):
+            sl.add(num)
+            if r-l >= k:
+                sl.remove(nums[l])
+                l+=1
+            if r-l == k-1:
+                results.append(sl[-1])
+        return results
